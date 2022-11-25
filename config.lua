c75fde567e112bddf783f2c386ef92c7fca043dd
Giovaahlua <119064647+Giovaahlua@users.noreply.github.com>
2022-11-25 16:34:48 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {} --This script is only Qb-Core
+
+Config.Notifica                                 = 'OKOK'
+
+Config.AliasComandoMoto                         = 'moto'
+Config.AliasComandoDespawn                      = 'despawnmoto'
+Config.TipoNotificaSpawnMoto                    = 'success'
+Config.TitoloSpawnMoto                          = 'Successo' -- REMEMBER: Only okokNotify
+Config.MessaggioSpawnMoto                       = 'Hai Spawnato una Moto!'
+Config.AliasComandoDespawn                      = 'despawnmoto'
+
+Config.TipoNotificaDespawnMoto                  = 'success'
+Config.TitoloDespawnMoto                        = 'Successo' -- REMEMBER: Only okokNotify
+Config.MessaggioDespawnMoto                     = 'Hai Despawnato la Moto!'
+
+Config.AliasComandoDespawnMoto                  = 'despawnmoto'
+Config.TastoDespawnMoto                         = 'F'
+
+Config.TipoNotificaDespawnFallitoMoto           = 'error'
+Config.TitoloDespawnFallitoMoto                 = 'Errore' -- REMEMBER: Only okokNotify
+Config.MessaggioDespawnFallitoMoto              = 'Non sei su una BF400!'
