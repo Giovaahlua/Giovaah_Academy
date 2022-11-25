2897ae4bfdb05a6ea37b6ac42a9b3fd59848a449
Giovaahlua <119064647+Giovaahlua@users.noreply.github.com>
2022-11-25 23:10:50 +0100

    Add files via upload
    update

--- config.lua
+++ config.lua
@@ -13,7 +13,7 @@ Config = {} -- DON'T TOUCH THIS!!
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-Config.Notifica                                 = 'QB'                        -- Notify System | 'OKOK' (okokNotify), 'QB' (QB-Core Default Notification System)
+Config.Notifica                                 = 'QB'                          -- Notify System | 'OKOK' (okokNotify), 'QB' (QB-Core Default Notification System)
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Start Moto System
@@ -21,32 +21,43 @@ Config.Notifica                                 = 'QB'                        --
 Config.ModelloHashVeicolo                       = 86520421                      -- Model Hash of Vehicles, you can see it from the /admin menù
 
 Config.AliasComandoSpawnMoto                    = 'moto'                        -- Alias of Command Spawn
+
 Config.TipoNotificaSpawnMoto                    = 'success'                     -- (success, error, info)
 Config.TitoloSpawnMoto                          = 'Successo'                    -- Title | REMEMBER: Only okokNotify
-Config.MessaggioSpawnMoto                       = 'Hai Spawnato una Moto!'      -- You have Spawned the Bike!
+Config.MessaggioSpawnMoto                       = 'Hai Spawnato una Moto!'      -- Message | You have Spawned the Bike!
 
-Config.TipoNotificaDespawnMoto                  = 'success'                     -- (success, error, info)
-Config.TitoloDespawnMoto                        = 'Successo'                    -- Title | REMEMBER: Only okokNotify
-Config.MessaggioDespawnMoto                     = 'Hai Despawnato la Moto!'     -- You have Despawned the Bike!
+--------------------------------------------------|
 
 Config.AliasComandoDespawnMoto                  = 'despawnmoto'                 -- Alias of Command Despawn
 Config.TastoDespawnMoto                         = 'F'                           -- Key for Execute the Command
+
+Config.TipoNotificaDespawnMoto                  = 'success'                     -- (success, error, info)
+Config.TitoloDespawnMoto                        = 'Successo'                    -- Title | REMEMBER: Only okokNotify
+Config.MessaggioDespawnMoto                     = 'Hai Despawnato la Moto!'     -- Message |  You have Despawned the Bike!
+
 
 -- End Bike System
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Start Ammo System
 
-Config.AliasComandoMunizioni                    = 'munizioni'                   -- Alias of Command Ammo
+Config.AliasComandoMunizioni                    = 'munizioni'                                                                                              -- Alias of Command Ammo
 
-Config.TipoNotificaRicaricaArmi                 = 'success'                     -- (success, error, info)
-Config.TitoloRicaricaArmi                       = 'Successo'                    -- Title | REMEMBER: Only okokNotify
-Config.MessaggioRicaricaArmi                    = 'Hai Ricaricato l\'Arma!'     -- You have Despawned the Bike!
+Config.TipoNotificaRicaricaArmi                 = 'success'                                                                                                -- (success, error, info)
+Config.TitoloRicaricaArmi                       = 'Successo'                                                                                               -- Title | REMEMBER: Only okokNotify
+Config.MessaggioRicaricaArmi                    = 'Hai Ricaricato l\'Arma!'                                                                                -- You have Reloaded the Weapon
 
-Config.ArmiAmmesse =  {                                                         -- Weapon Allowed
+Config.TipoNotificaRicaricaArmiFallito          = 'error'                                                                                                  -- (success, error, info)
+Config.TitoloRicaricaArmiFallito                = 'Errore'                                                                                                 -- Title | REMEMBER: Only okokNotify
+Config.MessaggioRicaricaArmiFallito             = 'Non puoi Ricaricare quest\'Arma! Credi che sia un Problema? Contatta l\'Amministratore del Server!'     -- Message | You can't Reload this Weapon! Do you think it is a Problem? Contact the Server Administrator
 
-    Armi = {                                                                    -- Weapon Allowed
-        'weapon_pistol',                                                        -- Weapon Allowed
-        'weapon_pistol50'                                                       -- Weapon Allowed
-    }                                                                           -- Weapon Allowed
+Config.ArmiAmmesse =  {                                                                                                                                    -- Weapon Allowed
 
-}                                                                               -- Weapon Allowed
+    Armi = {
+        'weapon_pistol',
+        'weapon_pistol50'
+    }       
+
+}           
+
+-- End Ammo System
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
