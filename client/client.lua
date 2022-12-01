c33706b024600ad98e3abd5f35386df73659e634
Giovaahlua <119064647+Giovaahlua@users.noreply.github.com>
2022-12-01 23:15:03 +0100

    Add files via upload

--- client/client.lua
+++ client/client.lua
@@ -52,6 +52,31 @@ RegisterCommand(Config.AliasComandoMunizioni, function()
     end
 end)
 
+if Config.DisattivaNPC == 'true' then
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(0)
+
+            SetVehicleDensityMultiplierThisFrame(0.0) -- set traffic density to 0
+			SetPedDensityMultiplierThisFrame(0.0) -- set npc/ai peds density to 0
+			SetRandomVehicleDensityMultiplierThisFrame(0.0) -- set random vehicles (car scenarios / cars driving off from a parking spot etc.) to 0
+			SetParkedVehicleDensityMultiplierThisFrame(0.0) -- set random parked vehicles (parked car scenarios) to 0
+			SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0) -- set random npc/ai peds or scenario peds to 0
+			SetGarbageTrucks(false) -- Stop garbage trucks from randomly spawning
+			SetRandomBoats(false) -- Stop random boats from spawning in the water.
+			SetCreateRandomCops(false) -- disable random cops walking/driving around.
+			SetCreateRandomCopsNotOnScenarios(false) -- stop random cops (not in a scenario) from spawning.
+			SetCreateRandomCopsOnScenarios(false) -- stop random cops (in a scenario) from spawning.
+
+			local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
+			ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
+			RemoveVehiclesFromGeneratorsInArea(x - 500.0, y - 500.0, z - 500.0, x + 500.0, y + 500.0, z + 500.0)
+        end
+    end)
+else
+end
+
+
 RegisterCommand('Giovaah_Academy', function()
     if Config.Notifica == 'OKOK' then
         TriggerEvent('okokNotify:Alert', '', 'Giovaah_Academy (1.0)', 10000, 'info')  
